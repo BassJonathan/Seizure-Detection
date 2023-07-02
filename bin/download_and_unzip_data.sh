cf99e172d84acd875fc5dc5ba0d8bc199ea8e2aa
Jonathan Bass <44204074+BassJonathan@users.noreply.github.com>
2023-07-02 18:34:39 +0200

    Add: Automatic download

---
+++ bin/download_and_unzip_data.sh
@@ -0,0 +1,3 @@
+curl https://physionet.org/static/published-projects/chbmit/chb-mit-scalp-eeg-database-1.0.0.zip > ../00_Data/chb-mit-scalp-eeg-database-1.0.0.zip
+
+unzip {chb-mit-scalp-eeg-database-1.0.0.zip}
